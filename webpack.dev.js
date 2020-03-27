f29edb11dbf8654183ee05d14c54585cce375589
Teodor Tsachev <teo.tsachev@gmail.com>
2020-03-27 16:47:41 +0200

    Install webpack and basic config
    -Installed webpack, webpack cli, webpack merge
    -Created config files common/dev/prod

---
+++ webpack.dev.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const common = require('./webpack.common');
+const merge = require('webpack-merge')
+
+module.exports = merge( common, {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build/assets'),
+    }
+});
